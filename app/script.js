94c11271bc06c9855a11f75f66d1c056b99fceff
THACHER <sophie.thacher@gmail.com>
2016-03-04 16:48:28 -0800

    form functions took a lot of time, plus did some AJAX on this branch too...

--- app/script.js
+++ app/script.js
@@ -1,1 +1,57 @@
-var submitButton = document.querySelectorByClass('')
+'use strict';
+$(document).ready(function(){
+    function eventHandler(event) {
+      var foodName = $('form label innerText')
+      console.log(foodName);
+      if($(this).is(':checked') === true){
+        console.log('checked!!!');
+        $( ".orderWindow" ).append();
+      } else {console.log('nothing')}
+      console.log()
+    }
+    $('form input').on('click', eventHandler);
+
+})
+
+
+$( document ).ready(function() {
+  $.ajax({
+    url: "https://galvanize-eats-api.herokuapp.com/menu",
+    method: "GET",
+    success: function (data){
+      console.log(data.menu);
+      var menu = data.menu;
+      // console.log(data.menu.id);
+
+      (data.menu).forEach( function (data) {
+        var $element = $('<p>');
+        $element.text(data.name + " ($" + data.price + ")");
+
+        // $element.text(data.price);
+        // $element.text(data.type);
+
+        $( ".menuDownload" ).append($element);
+
+        function menuClick (event) {
+          // $( ".orderWindow" ).append();
+          $( ".currentItem" ).append(data.name);
+          console.log(event);
+        }
+
+
+        $element.on('click', menuClick);
+      })
+    }
+  })
+});
+
+// $( document ).ready(function() {
+// $.ajax({
+//   method: "POST",
+//   url: "https://galvanize-eats-api.herokuapp.com/orders",
+//   data: { name: "Pizza", price: 10.99, type: "pizza" }
+// })
+//   .done(function( msg ) {
+//     alert( "Data Saved: " + msg );
+// });
+// });
