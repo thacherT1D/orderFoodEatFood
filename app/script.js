e824c2737d8cc261269f4d6dacb920c34ae5307a
THACHER <sophie.thacher@gmail.com>
2016-03-20 17:02:26 -0700

    changes to js file per linter

--- app/script.js
+++ app/script.js
@@ -1,81 +1,56 @@
-'use strict';
-
-$( document ).ready(function() {
+$(document).ready(function() {
+  'use strict';
   var currentItem;
   var currentItemPrice;
   var orderArray = [];
   var orderObject = {};
   var quanItem;
   var orderItem = {};
-  var itemQuanCost;
   var itemTotal;
 
   $.ajax({
-    url: "https://galvanize-eats-api.herokuapp.com/menu",
-    method: "GET",
-    success: function (data){
+    url: 'https://galvanize-eats-api.herokuapp.com/menu',
+    method: 'GET',
+    success: function(data) {
       var menu = data.menu;
-      // console.log(data.menu);
 
-      // var currentItem;
-      // var currentItemPrice;
-      // var orderArray = [];
-      // var orderObject = {};
-      // var quanItem;
-      // var orderItem = {};
-      // var itemQuanCost;
-      // var itemTotal;
-
-      (data.menu).forEach( function (data) {
-        // var $element = $('<p>');
-        // $('<p data-price="' + data.price + '">' + data.name + '</p>');
+      menu.forEach(function(data) {
         var $element = $('<p data-price="' + data.price + '">' + data.name + '</p>');
 
-        $( ".menuDownload" ).append($element.text(data.name));
-
+        $('.menuDownload').append($element.text(data.name));
       });
 
-      function menuClick (event) {
+      function menuClick(event) {
         currentItem = $(event.target).html();
-        // console.log(currentItem);
-        // console.log($(event.target).data('price'));
-
         currentItemPrice = $(event.target).data('price');
-        // console.log(currentItemPrice);
-
         $('.menuDownload').removeClass('highlightitem');
         $('p').removeClass('highlightitem');
         $(event.target).addClass('highlightitem');
         $('.menuDownload').removeClass('highlightitem');
+      }
+      $('.menuDownload').on('click', menuClick);
 
-      };
-      $( ".menuDownload" ).on('click', menuClick);
-
-      function addToOrder (event) {
+      function addToOrder(event) {
         event.preventDefault();
         quanItem = $('#quantity')[0].value;
         itemTotal = currentItemPrice * quanItem;
-
         orderItem = {
           name: currentItem,
           quantity: quanItem,
           price: currentItemPrice,
-          fullPrice: itemTotal,
+          fullPrice: itemTotal
         };
-
         orderArray.push(orderItem);
+        var ul = document.getElementById('list');
+        var li = document.createElement('li');
 
-        var ul = document.getElementById("list");
-        var li = document.createElement("li");
         li.appendChild(document.createTextNode(currentItem + ' (' + quanItem + ')'));
         ul.appendChild(li);
-
         $('p').removeClass('highlightitem');
         $('#quantity')[0].value = 1;
-      };
-      $( ".orderButton" ).on('click', addToOrder);
-
-      function deliverIt (event) {
+      }
+      $('.orderButton').on('click', addToOrder);
+      function deliverIt(event) {
         event.preventDefault();
         var person = {
           name: $('#name')[0].value,
@@ -83,25 +58,20 @@ $( document ).ready(function() {
           address: $('#address')[0].value,
           phoneNumber: $('#phoneNumber')[0].value
         };
-
         orderObject = {
           deliverTo: person,
-          food: orderArray,
-        }
-        console.log(orderObject);
+          food: orderArray
+        };
         $.ajax({
-          method: "POST",
-          url: "https://galvanize-eats-api.herokuapp.com/orders",
-          data: {order: orderObject},
+          method: 'POST',
+          url: 'https://galvanize-eats-api.herokuapp.com/orders',
+          data: { order: orderObject },
         })
-          .done(function( msg ) {
-            alert( "Data Saved: " + msg );
-        });
-//Add Form Reset
-      };
-      $( "#deliverItButton" ).on('click', deliverIt);
-
+          .done(function(msg) {
+            alert('Data Saved:' + msg);
+          });
+      }
+      $('#deliverItButton').on('click', deliverIt);
     }
-  })
-
+  });
 });
